3d0375568374cdac154635a9db248a783ce8caac
ajpelaez <ajpelaez@correo.ugr.es>
2018-04-16 22:07:42 +0200

    Agrega nuevos modelos a django admin

--- docencia/admin.py
+++ docencia/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Universidad, Titulacion, Asignatura, Profesor
+
+admin.site.register(Universidad)
+admin.site.register(Titulacion)
+admin.site.register(Asignatura)
+admin.site.register(Profesor)
--- docencia/models.py
+++ docencia/models.py
@@ -3,24 +3,24 @@ from django.db import models
 
 class Universidad(models.Model):
     nombre = models.CharField(max_length=100, primary_key=True)
-    logo = models.ImageField(upload_to="/static/", null=True, blank=True)
+    logo = models.ImageField(upload_to="static/", null=True, blank=True)
     web = models.CharField(max_length=200)
 
 
 class Titulacion(models.Model):
     nombre = models.CharField(max_length=100)
-    universidad = models.ForeignKey(Universidad)
+    universidad = models.ForeignKey(Universidad, on_delete=models.PROTECT)
 
 
 class Asignatura(models.Model):
     nombre = models.CharField(max_length=100)
-    universidad = models.ForeignKey(Universidad)
-    titulacion = models.ForeignKey(Titulacion)
+    universidad = models.ForeignKey(Universidad, on_delete=models.PROTECT)
+    titulacion = models.ForeignKey(Titulacion, on_delete=models.PROTECT)
 
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=100)
-    foto = models.ImageField(upload_to="/static/", null=True, blank=True)
-    asignaturas = models.ForeignKey(Asignatura)
-    universidad = models.ForeignKey(Universidad)
+    foto = models.ImageField(upload_to="static/", null=True, blank=True)
+    asignaturas = models.ForeignKey(Asignatura, on_delete=models.SET_NULL, null=True)
+    universidad = models.ForeignKey(Universidad, on_delete=models.PROTECT)
     email = models.EmailField(max_length=200)
