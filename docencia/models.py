a5689f97b2d3e363e93f57782cb6f9981319ecdb
ajpelaez <ajpelaez@correo.ugr.es>
2018-04-24 12:51:05 +0200

    Meta clase para los modelos

--- docencia/models.py
+++ docencia/models.py
@@ -4,12 +4,18 @@ from django.db import models
 class Universidad(models.Model):
     nombre = models.CharField(max_length=100, primary_key=True)
     logo = models.ImageField(upload_to="static/", null=True, blank=True)
-    web = models.CharField(max_length=200)
+    web = models.SlugField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Universidades"
 
 
 class Titulacion(models.Model):
     nombre = models.CharField(max_length=100)
     universidad = models.ForeignKey(Universidad, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name_plural = "Titulaciones"
 
 
 class Asignatura(models.Model):
@@ -17,10 +23,16 @@ class Asignatura(models.Model):
     universidad = models.ForeignKey(Universidad, on_delete=models.PROTECT)
     titulacion = models.ForeignKey(Titulacion, on_delete=models.PROTECT)
 
+    class Meta:
+        verbose_name_plural = "Asignaturas"
+
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=100)
     foto = models.ImageField(upload_to="static/", null=True, blank=True)
-    asignaturas = models.ForeignKey(Asignatura, on_delete=models.SET_NULL, null=True)
+    asignaturas = models.ManyToManyField(Asignatura, null=True)
     universidad = models.ForeignKey(Universidad, on_delete=models.PROTECT)
     email = models.EmailField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Profesores"
