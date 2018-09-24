248e1c15358ebd6e228f515786c5ec853f3c86c8
ajpelaez <ajpelaez@correo.ugr.es>
2018-09-24 23:15:01 +0200

    Registrar logs en heroku

--- tizanegra/settings.py
+++ tizanegra/settings.py
@@ -89,6 +89,23 @@ db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
