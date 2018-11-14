354c36ec079cdc64860ca576b407ec1140c6a779
ajpelaez <ajpelaez@correo.ugr.es>
2018-11-14 19:06:45 +0100

    Mejora sistema de etiquetas

--- tizanegra/utils.py
+++ tizanegra/utils.py
@@ -3,12 +3,31 @@ subject_tags = {
     "Desfasada": 0,
     "Actual": 0,
     "Muy práctica": 0,
-    "Muy teórica": 0
+    "Muy teórica": 0,
+    "Aburrida": 0,
+    "Entretenida": 0,
+    "Indescifrable": 0,
+    "Sencilla": 0,
+    "Mucha carga": 0,
+    "Ligera": 0
 }
 
 teacher_tags = {
     "Exigente": 0,
     "Justo": 0,
     "Pasota": 0,
-    "Simpático": 0
+    "Simpático": 0,
+    "Entregado": 0,
+    "Sociable": 0,
+    "Testarudo": 0,
+    "Soporífero": 0,
+    "Divertido": 0,
+    "Atento": 0,
+    "Arbitrario": 0,
+    "Quisquilloso": 0,
+    "Injusto": 0,
+    "Estricto": 0,
+    "Borde": 0,
+    "Desmotivado": 0,
+    "Cooperador": 0
 }
